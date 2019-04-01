6c52f1b500f40a1568aea6edb6b0e82efea8af8a
huijiangzhou <huijiangzhou@wecash.net>
2019-04-01 18:58:30 +0800

    first commit

---
+++ designpatternv1/src/main/java/com/huijz/net/strategy/IStrategy.java
@@ -0,0 +1,13 @@
+package com.huijz.net.strategy;
+
+/**
+ * @author huijz
+ * @date 2019/04/01
+ * 定义策略模式接口
+ */
+public interface IStrategy {
+    /**
+     * 策略的执行方法
+     */
+    public void operate();
+}
