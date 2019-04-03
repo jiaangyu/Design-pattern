e5084668b48e65c36e93894620bcb8aed0ffcaf6
huijiangzhou <huijiangzhou@wecash.net>
2019-04-03 18:26:58 +0800

    third commit

---
+++ designpatternv1/src/main/java/com/huijz/net/proxy/KindWoman.java
@@ -0,0 +1,17 @@
+package com.huijz.net.proxy;
+
+/**
+ * @author huijz
+ * @date 2019/04/03
+ */
+public interface KindWoman {
+    /**
+     * 抛媚眼
+     */
+    void makeEyesWithMan();
+
+    /**
+     * 嘿嘿嘿
+     */
+    void happyWithMan();
+}
